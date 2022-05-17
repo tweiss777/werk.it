86b215b52c2aac168ed30a82520cad737bb89253
Tal Weiss <Tweiss747@gmail.com>
2022-05-17 17:47:59 +0300

    added 3 options to the menubar

---
+++ client/src/components/MenuBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// this will be hooked to the navigation bar
+export default function MenuBar(){
+
+
+
+
+    return(
+        // flex box
+        <div className="menu-bar-container">
+            <div className="menu-bar">
+                <Link to='/saved'>
+                    <button>Saved Jobs</button>
+                </Link>
+                <Link to='/template'>
+                    <button>CV Template</button>
+                </Link>
+                <Link to='/profile'>
+                    <button>Profile Settings</button>
+                </Link>
+
+            </div>
+        </div>
+        )
+
+
+
+
+}
