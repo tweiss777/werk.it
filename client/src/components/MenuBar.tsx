c13da31e5359c7023be48f2276766b16474846f2
Tal Weiss <Tweiss747@gmail.com>
2022-05-20 14:33:10 +0300

    added user profile and name to menu bar

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -6,22 +6,28 @@ export default function MenuBar() {
     return (
         // flex box
         <div className="menu-bar-container">
-
+            
             <div className="menu-bar">
+                <div className="profile-info">
+                    <img src="https://ca.slack-edge.com/T02UUJ406K1-U030MDSN8CW-0ed4d6587952-512" alt="" />
+                    <p>Rolf Hegrand</p>
+
+                </div>
+
                 <Link to='/saved'>
-                    <div className="menuItem">
+                    <div className="menu-item">
                         <button>Saved Jobs</button>
                     </div>
                 </Link>
 
                 <Link to='/template'>
-                    <div className="menuItem">
+                    <div className="menu-item">
                         <button>CV Template</button>
                     </div>
                 </Link>
 
                 <Link to='/profile'>
-                    <div className="menuItem">
+                    <div className="menu-item">
                         <button>Profile Settings</button>
                     </div>
                 </Link>
