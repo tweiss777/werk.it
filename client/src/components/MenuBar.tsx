be376befe6493501f4c9d8860b53a10e97884d94
Tal Weiss <Tweiss747@gmail.com>
2022-05-20 19:54:58 +0300

    finished styling the menubar moving on to navbar

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/menubar.css';
-export default function MenuBar() {
+interface IProps{
+    isDisplayed?: boolean
+}
 
-    return (
+function MenuBar({isDisplayed}:IProps) {
+
+    if (!isDisplayed) return <></>
+    
+    else return (
+        
         // flex box
         <div className="menu-bar-container">
-<<<<<<< HEAD
-            
-=======
-            <div className="profile-info">
-                <img alt="donald" src="https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg" />
-            </div>
->>>>>>> fb3946781d2ba25c1098a174ebf3933b80ac8724
             <div className="menu-bar">
                 <div className="profile-info">
                     <img src="https://ca.slack-edge.com/T02UUJ406K1-U030MDSN8CW-0ed4d6587952-512" alt="" />
@@ -46,3 +46,5 @@ export default function MenuBar() {
 
 
 }
+
+export default MenuBar;
