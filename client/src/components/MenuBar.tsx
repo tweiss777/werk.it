fb3946781d2ba25c1098a174ebf3933b80ac8724
Tal Weiss <Tweiss747@gmail.com>
2022-05-20 13:26:06 +0300

    styling menu items and adding user image to menubar

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -6,22 +6,24 @@ export default function MenuBar() {
     return (
         // flex box
         <div className="menu-bar-container">
-
+            <div className="profile-info">
+                <img alt="donald" src="https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg" />
+            </div>
             <div className="menu-bar">
                 <Link to='/saved'>
-                    <div className="menuItem">
+                    <div className="menu-item">
                         <button>Saved Jobs</button>
                     </div>
                 </Link>
 
                 <Link to='/template'>
-                    <div className="menuItem">
+                    <div className="menu-item">
                         <button>CV Template</button>
                     </div>
                 </Link>
 
                 <Link to='/profile'>
-                    <div className="menuItem">
+                    <div className="menu-item">
                         <button>Profile Settings</button>
                     </div>
                 </Link>
