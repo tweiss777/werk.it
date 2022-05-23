517c21c2d4d0b011f4f8f05991741d656e136ba9
Tom Melloul <tambalul@gmail.com>
2022-05-23 16:56:33 +0300

    Fixed job's migration url fields defintion

--- server/src/migrations/Jobs.migration.js
+++ server/src/migrations/Jobs.migration.js
@@ -1,129 +1,128 @@
-import { Sequelize } from "sequelize";
-import db from "../data/Database.js";
-import Users from "./Auth.migration.js";
+import { Sequelize } from 'sequelize'
+import db from '../data/Database.js'
+import Users from './Auth.migration.js'
 
 const phaseValidator = [
-  "WISHLIST",
-  "NOT_STARTED",
-  "APPLICATION",
-  "HR_INTVW",
-  "TECH_INTVW",
-  "MANAGER_INTVW",
-  "HOME_ASGMT",
-  "OFFER",
-];
+    'WISHLIST',
+    'NOT_STARTED',
+    'APPLICATION',
+    'HR_INTVW',
+    'TECH_INTVW',
+    'MANAGER_INTVW',
+    'HOME_ASGMT',
+    'OFFER',
+]
 
-const statusValidator = ["COMPLETED", "INACTIVE", "IN_PROGRESS", "REJECTED"];
+const statusValidator = ['COMPLETED', 'INACTIVE', 'IN_PROGRESS', 'REJECTED']
 
-export const Jobs = db.define("jobs", {
-  id: {
-    type: Sequelize.UUID,
-    defaultValue: Sequelize.UUIDV4,
-    allowNull: false,
-    primaryKey: true,
-    validate: {
-      isUUID: {
-        args: 4,
-        msg: "Not a valid job ID",
-      },
+export const Jobs = db.define('jobs', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        validate: {
+            isUUID: {
+                args: 4,
+                msg: 'Not a valid job ID',
+            },
+        },
     },
-  },
-  added_by: {
-    type: Sequelize.UUID,
-    allowNull: false,
-    references: {
-      model: Users,
-      key: "id",
+    added_by: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+            model: Users,
+            key: 'id',
+        },
+        validate: {
+            isUUID: {
+                args: 4,
+                msg: 'Not a valid job ID',
+            },
+        },
     },
-    validate: {
-      isUUID: {
-        args: 4,
-        msg: "Not a valid job ID",
-      },
+    position: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'Software Developer',
+        validate: {
+            isAlpha: { msg: "jobs' position must be letters only" },
+        },
     },
-  },
-  position: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    defaultValue: "Software Developer",
-    validate: {
-      isAlpha: { msg: "jobs' position must be letters only" },
+    company_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isAlphanumeric: { msg: 'companyName must be alphanumeric' },
+        },
     },
-  },
-  company_name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isAlphanumeric: { msg: "companyName must be alphanumeric" },
+    company_url: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        validate: { isUrl: { message: 'URL is not valid' } },
     },
-  },
-  company_url: {
-    type: Sequelize.STRING,
-    validate: {
-      isUrl: { msg: "Not a valid URL" },
+    company_logo: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        validate: { isUrl: { message: 'URL is not valid' } },
     },
-  },
-  company_logo: {
-    type: Sequelize.STRING,
-    validate: {
-      isUrl: { msg: "Not valid URL" },
+    job_desc: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        validate: {
+            isAlphanumeric: {
+                msg: 'job_description must be alphanumeric',
+            },
+        },
     },
-  },
-  job_desc: {
-    type: Sequelize.STRING,
-    allowNull: true,
-    validate: {
-      isAlphanumeric: {
-        msg: "job_description must be alphanumeric",
-      },
+    job_source: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        validate: { isUrl: { message: 'URL is not valid' } },
     },
-  },
-  job_source: {
-    type: Sequelize.UUID,
-    isUrl: { msg: "Not valid URL" },
-  },
-  phase: {
-    type: Sequelize.STRING,
-    allowNull: true,
-    defaultValue: "NOT_STARTED",
-    validate: {
-      isIn: {
-        args: [phaseValidator],
-        msg: `Job phase must be ${phaseValidator}`,
-      },
+    phase: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: 'NOT_STARTED',
+        validate: {
+            isIn: {
+                args: [phaseValidator],
+                msg: `Job phase must be ${phaseValidator}`,
+            },
+        },
     },
-  },
-  status: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isIn: {
-        args: [statusValidator],
-        msg: `Job Status must be ${statusValidator}`,
-      },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isIn: {
+                args: [statusValidator],
+                msg: `Job Status must be ${statusValidator}`,
+            },
+        },
     },
-  },
-  handed_cv: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-    allowNull: false,
-  },
-  handed_asgmt: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-    allowNull: false,
-  },
-  handed_cover: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-    allowNull: false,
-  },
-  notes: {
-    type: Sequelize.STRING,
-    allowNull: true,
-  },
-});
+    handed_cv: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+    },
+    handed_asgmt: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+    },
+    handed_cover: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+    },
+    notes: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+})
 
-(async () => {
-  await db.sync();
-})();
+;(async () => {
+    await db.sync()
+})()
