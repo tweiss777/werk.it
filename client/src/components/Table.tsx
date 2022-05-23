af7b6b08f6a8ed8015e62eecf043391a75ef1e6d
Tal Weiss <Tweiss747@gmail.com>
2022-05-23 17:05:26 +0300

    fixed dashboard overflow and gave dashboard container a fixed height and set overflow x to scroll

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Filterrow from "./Filterrow";
 import Jobinfo from "./Jobinfo";
+import '../styles/jobInfo.css';
 
 export default function Table() {
   const array: number[] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
@@ -8,9 +9,14 @@ export default function Table() {
   return (
     <>
       <Filterrow />
+      <div className="job-info-container">
       {array.map((item, index) => {
         return <Jobinfo key={index} />;
-      })}
+      })}  
+      </div>
+      {/* {array.map((item, index) => {
+        return <Jobinfo key={index} />;
+      })} */}
     </>
   );
 }
