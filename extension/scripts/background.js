548a7e523a8a2386cd846684bd2a1ce64354d187
Tal Weiss <tweiss747@gmail.com>
2022-05-24 18:00:07 +0300

    updated manifest to point to the correct html directory

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 				conditions: [new chrome.declarativeContent.PageStateMatcher({
-					pageUrl: {hostEquals: 'www.website.com'},
+					pageUrl: {hostEquals: 'https://www.linkedin.com/'},
 				})
 			],
 		    actions: [new chrome.declarativeContent.ShowPageAction()]
