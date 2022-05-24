ddcd16b9585ce07e6245044f38a756ec7596376b
Rolf <rolfhegrand@gmail.com>
2022-05-24 11:52:07 +0300

    All wishlists -1 working, removed console.logs

--- server/src/middleware/VerifyToken.js
+++ server/src/middleware/VerifyToken.js
@@ -1,18 +1,16 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const { REFRESH_TOKEN_SECRET: refreshTokenSecret } = process.env
+const { REFRESH_TOKEN_SECRET: refreshTokenSecret } = process.env;
 
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization']
-    const token = authHeader?.split(' ')[1]
-    console.log('!!!', token)
-    if (token == null)
-        return res.status(401).json('No valid authorization header')
-    jwt.verify(token, refreshTokenSecret, (err, decoded) => {
-        console.log('!!!', token)
-        if (err) return res.status(403).json('Token is not valid')
-        console.log(decoded)
-        req.user = decoded.user
-        next()
-    })
-}
+  const authHeader = req.headers["authorization"];
+  const token = authHeader?.split(" ")[1];
+  if (token == null)
+    return res.status(401).json("No valid authorization header");
+  jwt.verify(token, refreshTokenSecret, (err, decoded) => {
+    if (err) return res.status(403).json("Token is not valid");
+
+    req.user = decoded.user;
+    next();
+  });
+};
--- server/src/routes/Jobs.route.js
+++ server/src/routes/Jobs.route.js
@@ -1,43 +1,43 @@
-import express from 'express'
+import express from "express";
 import {
-    addJob,
-    editJob,
-    getJob,
-    getAllJobs,
-    removeJob,
-    getWishlistJobs,
-    addWishlistJob,
-    getWishlistJob,
-} from '../controllers/Jobs.controller.js'
-import { verifyToken } from '../middleware/VerifyToken.js'
+  addJob,
+  editJob,
+  getJob,
+  getAllJobs,
+  removeJob,
+  getWishlistJobs,
+  addWishlistJob,
+  getWishlistJob,
+} from "../controllers/Jobs.controller.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
 
-const router = express.Router() // "/pets"
+const router = express.Router(); // "/pets"
 
 // Get All jobs
 router
-    .route('/')
-    .get(verifyToken, getAllJobs)
-    // Add new job
-    .post(verifyToken, addJob)
+  .route("/")
+  .get(verifyToken, getAllJobs)
+  // Add new job
+  .post(verifyToken, addJob);
 
 // Get Job
 router
-    .route('/:id')
-    .get(verifyToken, getJob)
-    // Update Job
-    .put(verifyToken, editJob)
-    // Delete Job
-    .delete(verifyToken, removeJob)
+  .route("/:id")
+  .get(verifyToken, getJob)
+  // Update Job
+  .put(verifyToken, editJob)
+  // Delete Job
+  .delete(verifyToken, removeJob);
 
 // Get All Wishlist Jobs
 router
-    .route('/wishlist')
-    .get(verifyToken, getWishlistJobs)
-    // Save Wishlist Job
-    .post(verifyToken, addWishlistJob)
+  .route("/wishlist")
+  .get(verifyToken, getWishlistJobs)
+  // Save Wishlist Job
+  .post(verifyToken, addWishlistJob);
 // Get Wishlist job by id
-router.route('/wishlist/:id').get(verifyToken, getWishlistJob)
+router.route("/wishlist/:id").get(verifyToken, getWishlistJob);
 
-// 
+//
 
-export default router
+export default router;
