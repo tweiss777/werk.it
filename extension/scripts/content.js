11a499c74d27d29ebd3caed6910515d893509ac7
Tal Weiss <tweiss747@gmail.com>
2022-05-24 20:15:43 +0300

    managed to pull data from dom

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -2,8 +2,3 @@ let page_title = document.title,
 	page_h1_tag = '';
 const header2 = document.getElementsByTagName('h2');
 console.log(header2)
-// prepare JSON data with page title & first h1 tag
-let data = JSON.stringify({ title: page_title, h1: page_h1_tag });
-
-// send message back to popup script
-chrome.runtime.sendMessage(null, data);
--- extension/views/popup.js
+++ extension/views/popup.js
@@ -1,6 +1,9 @@
 'use strict';
 
 
+
+
+
 let url = chrome.extension.getBackgroundPage()
 // list of urls to navigate
 let urls_list = [
@@ -11,68 +14,61 @@ let urls_list = [
 	'https://website.com/page-5',
 ];
 
-// start navigation when #startNavigation button is clicked
-startNavigation.onclick = function(element) {
-	console.log("fuck you")
-	// query the current tab to find its id
-	chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
-		for(let i=0; i<urls_list.length; i++) {
-			// navigate to next url
-			await goToPage(urls_list[i], i+1, tabs[0].id);
-			
-			// wait for 5 seconds
-			await waitSeconds(5);
-		}
 
-		// navigation of all pages is finished
-		alert('Navigation Completed');
-	});
-};
 
-async function goToPage(url, url_index, tab_id) {
-	return new Promise(function(resolve, reject) {
-		// update current tab with new url
-		chrome.tabs.update({url: url});
-		
-		// fired when tab is updated
-		chrome.tabs.onUpdated.addListener(function openPage(tabID, changeInfo) {
-			// tab has finished loading, validate whether it is the same tab
-			if(tab_id == tabID && changeInfo.status === 'complete') {
-				// remove tab onUpdate event as it may get duplicated
-				chrome.tabs.onUpdated.removeListener(openPage);
-
-				// fired when content script sends a message
-				chrome.runtime.onMessage.addListener(function getDOMInfo(message) {
-					// remove onMessage event as it may get duplicated
-					chrome.runtime.onMessage.removeListener(getDOMInfo);
-
-					// save data from message to a JSON file and download
-					let json_data = {
-						title: JSON.parse(message).title,
-						h1: JSON.parse(message).h1,
-						url: url
-					};
-
-					let blob = new Blob([JSON.stringify(json_data)], {type: "application/json;charset=utf-8"});
-					let objectURL = URL.createObjectURL(blob);
-					chrome.downloads.download({ url: objectURL, filename: ('content/' + url_index + '/data.json'), conflictAction: 'overwrite' });
-				});
-
-				// execute content script
-				chrome.tabs.executeScript({ file: 'script.js' }, function() {
-					// resolve Promise after content script has executed
-					resolve();
-				});
-			}
-		});
-	});
+scrape.onclick = function() {
+	chrome.tabs.query({active:true},function(tabs){
+		let tab = tabs[0]
+		chrome.scripting.executeScript({
+			target: {tabId: tab.id},
+			function: scrapedata,
+		})
+	})
 }
 
-// async function to wait for x seconds 
-async function waitSeconds(seconds) {
-	return new Promise(function(resolve, reject) {
-		setTimeout(function() {
-			resolve();
-		}, seconds*1000);
-	});
+
+function scrapedata(event){
+	const cards = document.querySelector('.job-card-container')
+
+	try{
+
+	}
+	catch(err){
+
+	}
+
+	const company_url = window.location.href
+	console.log(company_url)
+	const position = document.querySelector('.t-24').innerText;
+	console.log(position)
+	const company_name = document.querySelector('.jobs-unified-top-card__company-name').innerText
+	console.log(company_name)
+	const job_desc = document.querySelector('.jobs-box__html-content').innerText
+	console.log(job_desc)
+
+
 }
+
+
+chrome.tabs.onActivated.addListener( function(activeInfo){
+	chrome.tabs.get(activeInfo.tabId, function(tab){
+		const y = tab.url; 
+		console.log("you are here: "+y);
+	});
+})();
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+	if (tab.active && change.url) {
+		console.log("you are here: "+change.url);           
+	}
+})();	
+
+
+// // async function to wait for x seconds 
+// async function waitSeconds(seconds) {
+// 	return new Promise(function(resolve, reject) {
+// 		setTimeout(function() {
+// 			resolve();
+// 		}, seconds*1000);
+// 	});
+// }
