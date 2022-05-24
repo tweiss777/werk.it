e3b3c841ed03436935940fb12a08af075b976603
Rolf <rolfhegrand@gmail.com>
2022-05-24 14:56:04 +0300

    getAllJobs function

--- client/src/components/JobsContextProvider.tsx
+++ client/src/components/JobsContextProvider.tsx
@@ -1,7 +1,26 @@
 import JobsContext from "../context/JobsContext";
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function JobsContextProvider({ children }) {
-  const [testValue, setTestValue] = useState("Test value string");
-  return <JobsContext.Provider value={{testValue}}>{children}</JobsContext.Provider>;
+  const PORT = 9000;
+  const SERVER_HOST = "localhost";
+  const serverURL = `http://${SERVER_HOST}:${PORT}`;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function getAllJobs() {
+      setIsLoading(true);
+    try {
+      const res = await axios.get(`${serverURL}/jobs`);
+      console.log("res.data", res.data);
+      setIsLoading(false);
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  }
+  getAllJobs();
+  return <JobsContext.Provider value={{}}>{children}</JobsContext.Provider>;
 }
